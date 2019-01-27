9d3a67cbdd5c7adb5aeacdb31a22695c8e361413
Nichham1 <nichham@gmail.com>
2019-01-26 23:36:10 -0500

    Add project files.

---
+++ Tunaconsoleapp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tunaconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            
+
+            Console.WriteLine("Just write your name:");
+          
+            name = Console.ReadLine();
+
+            Console.WriteLine("Please enter you Age:");
+
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your name is: " + name + " and you age is: "+ age);
+
+            Console.WriteLine($"Your name is {name} and" +
+                $"you are {age} years old: ");
+
+            Console.ReadLine();
+          
+        }
+    }
+}
